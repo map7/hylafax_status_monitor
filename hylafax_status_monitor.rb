9b3c887ccb3a4ca2b06ff3e92670690f122f6679
Michael Pope <michael@dtcorp.com.au>
2014-04-07 15:27:57 +1000

    Send email if automatically fixed

--- hylafax_status_monitor.rb
+++ hylafax_status_monitor.rb
@@ -4,6 +4,16 @@ require_relative 'lib/status.rb'
 require 'json'
 require 'mail'
 
+# Set up delivery defaults to use inhouse mail server
+Mail.defaults do
+  delivery_method :smtp, {
+    :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE, 
+    :address => 'mail.lan',
+    :port => '587',
+    :enable_starttls_auto => true
+  }
+end
+
 # Read in settings
 begin
   serialized = File.read("/etc/hylafax_status_monitor.json")
@@ -27,22 +37,23 @@ end
 
 if get_status
   puts "Fax is running successfully"
+
+  if sent_status(true) == true
+    # If we have a problem email the @output to my email address
+    Mail.deliver do
+      from email
+      to email
+      subject "Fax is back up"
+      body @output
+    end
+  end
+  
   set_status(false) # Record that we haven't sent any error messages
 else
   puts "Fax is down still down\n"
   puts @output
 
   if sent_status(false) == false
-    # Set up delivery defaults to use inhouse mail server
-    Mail.defaults do
-      delivery_method :smtp, {
-        :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE, 
-        :address => 'mail.lan',
-        :port => '587',
-        :enable_starttls_auto => true
-      }
-    end
-    
     # If we have a problem email the @output to my email address
     Mail.deliver do
       from email
