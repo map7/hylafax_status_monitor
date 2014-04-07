0c43ccde1849510ded61412c88e5140129bc0ec4
Michael Pope <michael@dtcorp.com.au>
2014-04-07 16:11:19 +1000

    Add 5 minutes to the stored time

--- lib/status.rb
+++ lib/status.rb
@@ -28,7 +28,7 @@ def status(text)
       store.transaction {store[:timed_status] = Time.now}
       true
 
-    elsif Time.now > timed_status
+    elsif Time.now > timed_status + 300
       # Check if this same error has been happening for more than 5 minutes.
       puts "reset timed_status"
       store.transaction {store[:timed_status] = nil}
